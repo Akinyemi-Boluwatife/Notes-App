5c0bf5b71e5f0fab63bd6e985adfbe121fed1281
Akinyemi-Boluwatife <boluintech@gmail.com>
2024-11-27 17:31:14 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+const list = [
+  {
+    id: 1,
+    Type: "Todo",
+    Title: "School Assignment",
+    content:
+      "The physics assignment is due on wednessday and it's course CODE IS PHY102, it is a 3 units course",
+  },
+  {
+    id: 2,
+    Type: "Note",
+    Title: "chm102",
+    content:
+      "The topic is about organic and inorganic chemistry and it includes all the functions and their uses",
+  },
+  {
+    id: 3,
+    Type: "Note",
+    Title: "csc102",
+    content:
+      "The topic is about introduction to algorithms and it specifies their uses and how to write it efficiently ",
+  },
+];
 function App() {
   return (
     <div>
       <Header />
       <Search />
+      <Form />
     </div>
   );
 }
@@ -52,9 +76,22 @@ function Search() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="search" onSubmit={handleSubmit}>
       <input type="text" placeholder="Search for notes/todo"></input>
       <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="form-inp" onSubmit={handleSubmit}>
+      <textarea placeholder="write........." cols="35" rows="7"></textarea>
+      <button className="btn-note-list">+</button>
     </form>
   );
 }
